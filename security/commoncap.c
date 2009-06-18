cc97640058292f40749734780c5543f2f69dedd7
Chia-chi Yeh <chiachi@android.com>
2009-06-19 07:15:05 +0800

    security: Add AID_NET_RAW and AID_NET_ADMIN capability check in cap_capable().
    Signed-off-by: Chia-chi Yeh <chiachi@android.com>
    
    merged with resolved conflicts
    Signed-off-by: Peter Jin <peter.zjin@gmail.com>

--- security/commoncap.c
+++ security/commoncap.c
@@ -44,6 +44,10 @@ EXPORT_SYMBOL(cap_bset);
 unsigned securebits = SECUREBITS_DEFAULT; /* systemwide security settings */
 EXPORT_SYMBOL(securebits);
 
+#ifdef CONFIG_ANDROID_PARANOID_NETWORK
+#include <linux/android_aid.h>
+#endif
+
 int cap_netlink_send(struct sock *sk, struct sk_buff *skb)
 {
 	NETLINK_CB(skb).eff_cap = current->cap_effective;
@@ -67,6 +71,13 @@ EXPORT_SYMBOL(cap_netlink_recv);
  */
 int cap_capable (struct task_struct *tsk, int cap)
 {
+#ifdef CONFIG_ANDROID_PARANOID_NETWORK
+	if (cap == CAP_NET_RAW && in_egroup_p(AID_NET_RAW))
+		return 0;
+	if (cap == CAP_NET_ADMIN && in_egroup_p(AID_NET_ADMIN))
+		return 0;
+#endif
+
 	/* Derived from include/linux/sched.h:capable. */
 	if (cap_raised(tsk->cap_effective, cap))
 		return 0;
