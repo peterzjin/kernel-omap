5e0760f089374557fb89935ca11d2d575582d925
San Mehat <san@google.com>
2009-08-13 09:40:42 -0700

    drivers: power: Add watchdog timer to catch drivers which lockup during suspend.
    	Rather than hard-lock the kernel, we now BUG() when a driver takes
    > 3 seconds to suspend. If the underlying platform supports panic dumps,
    then the data can be collected for debug.
    
    Signed-off-by: San Mehat <san@google.com>
    
    merged with resolved conflicts
    Signed-off-by: Peter Jin <peter.zjin@gmail.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -24,6 +24,7 @@
 #include <linux/mutex.h>
 #include <linux/pm.h>
 #include <linux/resume-trace.h>
+#include <linux/timer.h>
 
 #include "../base.h"
 #include "power.h"
@@ -37,6 +38,8 @@ static DEFINE_MUTEX(dpm_list_mtx);
 
 int (*platform_enable_wakeup)(struct device *dev, int is_on);
 
+static void dpm_drv_timeout(unsigned long data);
+static DEFINE_TIMER(dpm_drv_wd, dpm_drv_timeout, 0, 0);
 
 void device_pm_add(struct device *dev)
 {
@@ -98,6 +101,44 @@ static int resume_device(struct device * dev)
 	return error;
 }
 
+/**
+ *	dpm_drv_timeout - Driver suspend / resume watchdog handler
+ *	@data: struct device which timed out
+ *
+ * 	Called when a driver has timed out suspending or resuming.
+ * 	There's not much we can do here to recover so
+ * 	BUG() out for a crash-dump
+ *
+ */
+static void dpm_drv_timeout(unsigned long data)
+{
+	struct device *dev = (struct device *) data;
+
+	printk(KERN_EMERG "**** DPM device timeout: %s (%s)\n", dev_name(dev),
+	       (dev->driver ? dev->driver->name : "no driver"));
+	BUG();
+}
+
+/**
+ *	dpm_drv_wdset - Sets up driver suspend/resume watchdog timer.
+ *	@dev: struct device which we're guarding.
+ *
+ */
+static void dpm_drv_wdset(struct device *dev)
+{
+	dpm_drv_wd.data = (unsigned long) dev;
+	mod_timer(&dpm_drv_wd, jiffies + (HZ * 3));
+}
+
+/**
+ *	dpm_drv_wdclr - clears driver suspend/resume watchdog timer.
+ *	@dev: struct device which we're no longer guarding.
+ *
+ */
+static void dpm_drv_wdclr(struct device *dev)
+{
+	del_timer_sync(&dpm_drv_wd);
+}
 
 static int resume_device_early(struct device * dev)
 {
@@ -319,7 +360,9 @@ int device_suspend(pm_message_t state)
 		get_device(dev);
 		mutex_unlock(&dpm_list_mtx);
 
+		dpm_drv_wdset(dev);
 		error = suspend_device(dev, state);
+		dpm_drv_wdclr(dev);
 
 		mutex_lock(&dpm_list_mtx);
 
